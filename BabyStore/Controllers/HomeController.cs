8a84d26804962549b8bdf000b32d2dd1e865f216
stefaleon <stefaleon@gmail.com>
2018-02-07 09:10:29 +0200

    initial

--- BabyStore/Controllers/HomeController.cs
+++ BabyStore/Controllers/HomeController.cs
@@ -13,9 +13,9 @@ namespace BabyStore.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult About(string id)
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page. Got s: " + id;
 
             return View();
         }
